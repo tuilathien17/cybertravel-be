0931f92f5568f32847e4758dd71cfbd8e82b6e25
thien <thienthenguyen123@gmail.com>
2022-05-08 13:50:11 +0700

    remove 'csrf disabled'

--- src/main/java/com/cybergang/cybertravel/config/SecurityConfiguration.java
+++ src/main/java/com/cybergang/cybertravel/config/SecurityConfiguration.java
@@ -29,7 +29,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
                 .authorizeRequests().anyRequest().permitAll()
                 .and()
                 .httpBasic()
